002e3acf3d738db80688cf2fb793fc04f57437ef
GerardoMP <43628789+GerardoMP18@users.noreply.github.com>
2022-10-23 21:40:59 -0500

    [feat] Adding new package json

--- src/App.jsx
+++ src/App.jsx
@@ -9,31 +9,35 @@ import { Admin, A1, A2 } from "./pages/Admin/Admin";
 import { Propietario, P1, P2 } from "./pages/Propietario/Propietario";
 import { Inquilino, I1, I2 } from "./pages/Inquilino/Inquilino";
 import { Configuracion } from "./pages/Configuracion/Configuracion";
+import { CargaMasiva } from "./pages/CargaMasiva/CargaMasiva"
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Sidebar/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/condominios" element={<Condominios />} />
-          <Route path="/condominios/c1" element={<C1 />} />
-          <Route path="/condominios/c2" element={<C2 />} />
-          <Route path="/edificios" element={<Edificios />} />
-          <Route path="/edificios/e1" element={<E1 />} />
-          <Route path="/edificios/E2" element={<E2 />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/admin/a1" element={<A2 />} />
-          <Route path="/admin/a2" element={<A1 />} />
-          <Route path="/propietario" element={<Propietario />} />
-          <Route path="/propietario/p1" element={<P1 />} />
-          <Route path="/propietario/p2" element={<P2 />} />
-          <Route path="/inquilino" element={<Inquilino />} />
-          <Route path="/inquilino/i1" element={<I1 />} />
-          <Route path="/inquilino/i2" element={<I2 />} />
-          <Route path="/configuracion" element={<Configuracion />} />
-        </Routes>
+        <Sidebar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/condominios" element={<Condominios />} />
+            <Route path="/condominios/c1" element={<C1 />} />
+            <Route path="/condominios/c2" element={<C2 />} />
+            <Route path="/edificios" element={<Edificios />} />
+            <Route path="/edificios/e1" element={<E1 />} />
+            <Route path="/edificios/E2" element={<E2 />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/admin/a1" element={<A2 />} />
+            <Route path="/admin/a2" element={<A1 />} />
+            <Route path="/propietario" element={<Propietario />} />
+            <Route path="/propietario/p1" element={<P1 />} />
+            <Route path="/propietario/p2" element={<P2 />} />
+            <Route path="/inquilino" element={<Inquilino />} />
+            <Route path="/inquilino/i1" element={<I1 />} />
+            <Route path="/inquilino/i2" element={<I2 />} />
+            <Route path="/configuracion" element={<Configuracion />} />
+            <Route path="/cargamasiva" element={<CargaMasiva />} />
+          </Routes>
+        </div>
       </BrowserRouter>
     </>
   );
