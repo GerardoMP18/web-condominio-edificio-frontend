dd0bc9c865258e0ce657698052099b661c7baeb9
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-11-03 13:36:33 -0500

    [feat] adding functions that allow viewing, updating and deleting condominium data

--- src/components/Tables/TableCondominiums/TableCondominiums.jsx
+++ src/components/Tables/TableCondominiums/TableCondominiums.jsx
@@ -1,9 +1,12 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect, Fragment} from "react";
 import { STable, STBody, STBodyTR, STD, STH, STHead, STHeadTR, Container, SubContainer, Title } from "../../Tables/styles.js"
 import Navigation from "../../Navigation/Navigation.jsx";
 import { BsPencilSquare, BsTrashFill } from "react-icons/bs";
+import ReadOnlyRow from "./ReadOnlyRow";
+import EditableRow from "./EditableRow";
+import axios from "axios";
 
-const keys= ["name", "address", "phone", "email", "ruc"];
+const baseUrl = "http://127.0.0.1:5000/api/condominiums/";
 
 function TableCondominiums(props) {
   const { data } = props;
@@ -11,6 +14,22 @@ function TableCondominiums(props) {
   const [pageCount, setPageCount] = useState(0);
   const [itemOffset, setItemOffset] = useState(0);
   const itemsPerPage = 11;
+
+/* editFormData tiene los datos para enviar al metodo PUT */
+  const [editFormData, setEditFormData] = useState({
+    name: "",
+    ruc: "",
+    phone: "",
+    email: "",
+    address: "",
+    landline: "",
+    description: "",
+    user_created: "",
+    user_updated: "",
+  });
+
+/* editContactId tiene el ID de el Row que voy a actualizar */
+  const [editContactId, setEditContactId] = useState(null);
   
   useEffect(() => {
     console.log("Paso 6")
@@ -23,45 +42,133 @@ function TableCondominiums(props) {
     const newOffset = (event.selected * itemsPerPage) % data.length;
     setItemOffset(newOffset);
   };
-  
+
+/* Funcion que al escribir nuevos datos en EditableRow, se almacenen aqui y se llenen en EditFormData */
+  const handleEditFormChange = (event) => {
+    event.preventDefault();
+
+    const fieldName = event.target.getAttribute("name");
+    const fieldValue = event.target.value;
+
+    const newFormData = { ...editFormData };
+    newFormData[fieldName] = fieldValue;
+
+    setEditFormData(newFormData);
+  }
+
+/* funcion que sobreescribira la columna seleccionada con sus datos actualizados y tambien acciona al metodo PUT para actualizar en la base de datos*/
+  const handleEditFormSubmit = (event) => {
+    event.preventDefault();
+
+    const editedCondominium = {
+      id: editContactId,
+      name: editFormData.name,
+      ruc: editFormData.ruc,
+      phone: editFormData.phone,
+      email: editFormData.email,
+      address: editFormData.address,
+      landline: editFormData.landline,
+      description: editFormData.description,
+    }
+
+    const newCondominiums = [...currentItems]
+
+    const index = currentItems.findIndex((condominium)=> condominium.id === editContactId);
+
+    newCondominiums[index] = editedCondominium;
+
+    axios.put(baseUrl + editContactId, editFormData).then((response) => {
+      response.status === 200 ? (
+	setCurrentItems(newCondominiums),
+      ) : (
+        alert("NO FUNCIONA")
+      )
+    });
+    setEditContactId(null);
+  };
+
+/* Funcion que al hacer click al boton editar, le dara el id del condominio a editar para activar el EditableRow */
+  const handleEditClick = (event, condominium) => {
+    event.preventDefault();
+    setEditContactId(condominium.id);
+
+    const formValues = {
+      name: condominium.name,
+      ruc: condominium.ruc,
+      phone: condominium.phone,
+      email: condominium.email,
+      address: condominium.address,
+      landline: condominium.landline,
+      description: condominium.description,
+    }
+
+    setEditFormData(formValues);
+  };
+
+/* Funcion que cancela la actualizacion de un row */
+  const handleCancelClick = () => {
+    setEditContactId(null);
+  }
+
+/* Funcion que elimina un Row y hace uso de el METODO DELETE*/
+  const handleDeleteClick = (condominiumId) => {
+    const newCondominiums = [...currentItems]
+
+    const index = currentItems.findIndex((condominium)=> condominium.id === condominiumId);
+
+    newCondominiums.splice(index, 1)
+
+    axios.delete(baseUrl + condominiumId).then((response) => {
+      response.status === 200 ? (
+        setCurrentItems(newCondominiums),
+      ) : (
+        alert("NO FUNCIONA")
+      )
+    });
+  }
+
   console.log(data)
   return (
     <Container>
       <SubContainer>
       <Title>Lista de Condominios</Title>
-      <STable>
-        <STHead>
-          <STHeadTR>
-            {["#", ...keys].map((item, index) => (
-              <STH key={index}>
-                {item}
-              </STH>
+      <form>
+        <STable>
+          <STHead>
+            <STHeadTR>
+	      <STH>#</STH>
+	      <STH>name</STH>
+              <STH>address</STH>
+	      <STH>phone</STH>
+	      <STH>email</STH>
+	      <STH>ruc</STH>
+              <STH>Acciones</STH>
+            </STHeadTR>
+          </STHead>
+          <STBody>
+            {currentItems.map((condominium, index) => (
+              <Fragment>
+		{ editContactId === condominium.id ? (
+		  <EditableRow
+                    editFormData={editFormData}
+		    index={index}
+		    handleEditFormChange={handleEditFormChange}
+		    handleEditFormSubmit={handleEditFormSubmit}
+		    handleCancelClick={handleCancelClick}
+		  />
+		) : (
+	          <ReadOnlyRow
+		    condominium={condominium}
+		    index={index}
+		    handleEditClick={handleEditClick}
+	            handleDeleteClick={handleDeleteClick}
+		  />
+		)}
+	      </Fragment>
             ))}
-            <STH>Acciones</STH>
-          </STHeadTR>
-        </STHead>
-        <STBody>
-          {currentItems.map((obj, index) => (
-            <STBodyTR key={index}>
-              <STD>
-                {index + 1}
-              </STD>
-              {keys.map((item, index) => {
-                const value = obj[item];
-                return (
-                <STD key={index}>
-                  {value}
-                </STD>
-                );
-              })}
-              <STD>          
-                <BsPencilSquare style={{ color: "#416AF9",fontSize: "25px", marginRight: "10px", cursor: "pointer" }} />
-                <BsTrashFill style={{ color: "#ff0000", fontSize: "25px", cursor: "pointer" }}/> 
-              </STD>
-            </STBodyTR>
-          ))}
-        </STBody>
-      </STable>
+          </STBody>
+        </STable>
+      </form>
       <Navigation
         pageClick={handlePageClick}
         contador={pageCount}
